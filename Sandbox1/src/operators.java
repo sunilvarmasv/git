93d9ef718c70cadd1437761f5c63555e3ccc1649
sunilvarmasv <33340008+sunilvarmasv@users.noreply.github.com>
2018-02-16 18:15:00 +0530

    fdsf

---
+++ Sandbox1/src/operators.java
@@ -0,0 +1,47 @@
+
+public class operators {
+	
+	public static void main(String args[]){
+		
+		int a =1 ;
+		int b =2;
+		
+		// = is always assingment operator
+		// == comparsion
+		// +,-,/,*,%
+		
+		// ++ , -- 
+		
+		// a++ Post increment
+  // and ++a pre increment
+		
+		//System.out.println(a++); 
+		//System.out.println(a); 
+			
+		//System.out.println(++a);	
+		
+		//System.out.println(10*10/5+3-1*4/2);
+				
+		
+		{  
+			int x=10;  
+			System.out.println(x++);  // x++ means x+1 ; 10+1 = 11
+			System.out.println(++x);  // ++x 11+1 =12 ; 
+			System.out.println(x--);  // 12 ; 12-- = 11
+			System.out.println(--x); // --11 = 10
+			}  
+		
+		
+				
+	}
+
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+}
